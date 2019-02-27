6c1a3956b8c1bfe028c23b10f553e59a0d602972
Junichi Kato <j5ik2o@gmail.com>
2019-02-27 22:43:16 +0900

    Refactor

--- src/main/scala/com/github/j5ik2o/akka/persistence/dynamodb/serialization/FlowPersistentReprSerializer.scala
+++ src/main/scala/com/github/j5ik2o/akka/persistence/dynamodb/serialization/FlowPersistentReprSerializer.scala
@@ -7,20 +7,17 @@ import akka.stream.scaladsl.Flow
 trait FlowPersistentReprSerializer[T] extends PersistentReprSerializer[T] {
 
   def deserializeFlow: Flow[T, (PersistentRepr, Set[String], Long), NotUsed] = {
-    Flow[T].map(
-      v =>
-        deserialize(v) match {
-          case Right(r) => r
-          case Left(ex) => throw ex
-      }
-    )
+    Flow[T].map(deserialize).map {
+      case Right(r) => r
+      case Left(ex) => throw ex
+    }
   }
 
   def deserializeFlowWithoutTags: Flow[T, PersistentRepr, NotUsed] = {
-    def keepPersistentRepr(tup: (PersistentRepr, Set[String], Long)): PersistentRepr = tup match {
-      case (repr, _, _) => repr
+    deserializeFlow.map {
+      case (repr, _, _) =>
+        repr
     }
-    deserializeFlow.map(keepPersistentRepr)
   }
 
 }

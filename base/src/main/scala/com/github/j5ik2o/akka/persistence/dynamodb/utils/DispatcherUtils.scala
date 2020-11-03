299f2a9e017bd39cd3c65e9de383b3600caf4d25
Junichi Kato <j5ik2o@gmail.com>
2020-11-04 00:11:02 +0900

    Fix DispatcherUtils.scala.

--- base/src/main/scala/com/github/j5ik2o/akka/persistence/dynamodb/utils/DispatcherUtils.scala
+++ base/src/main/scala/com/github/j5ik2o/akka/persistence/dynamodb/utils/DispatcherUtils.scala
@@ -9,19 +9,29 @@ import com.github.j5ik2o.akka.persistence.dynamodb.config.client.ClientVersion
 object DispatcherUtils extends LoggingSupport {
 
   def applyV1Dispatcher[A, B](pluginConfig: PluginConfig, flow: Flow[A, B, NotUsed]): Flow[A, B, NotUsed] = {
-    (if (pluginConfig.clientConfig.clientVersion == ClientVersion.V1)
-       pluginConfig.clientConfig.v1ClientConfig.dispatcherName
-     else
-       pluginConfig.clientConfig.v1DaxClientConfig.dispatcherName)
-      .fold {
-        flow
-      } { name => flow.withAttributes(ActorAttributes.dispatcher(name)) }
+    val dispatcherName = pluginConfig.clientConfig.clientVersion match {
+      case ClientVersion.V1 =>
+        pluginConfig.clientConfig.v1ClientConfig.dispatcherName
+      case ClientVersion.V1Dax =>
+        pluginConfig.clientConfig.v1DaxClientConfig.dispatcherName
+      case _ =>
+        throw new IllegalArgumentException("Invalid the client version")
+    }
+    dispatcherName.fold(
+      flow
+    ) { name => flow.withAttributes(ActorAttributes.dispatcher(name)) }
   }
 
   def applyV2Dispatcher[A, B](pluginConfig: PluginConfig, flow: Flow[A, B, NotUsed]): Flow[A, B, NotUsed] = {
-    pluginConfig.clientConfig.v2ClientConfig.dispatcherName.fold(flow) { name =>
-      flow.withAttributes(ActorAttributes.dispatcher(name))
+    val dispatcherName = pluginConfig.clientConfig.clientVersion match {
+      case ClientVersion.V2 =>
+        pluginConfig.clientConfig.v2ClientConfig.dispatcherName
+      case _ =>
+        throw new IllegalArgumentException("Invalid the client version")
     }
+    dispatcherName.fold(
+      flow
+    ) { name => flow.withAttributes(ActorAttributes.dispatcher(name)) }
   }
 
 }

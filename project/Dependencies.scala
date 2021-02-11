c328442934cf3d7a43c984418d9f258b80c472fe
Scala Steward <me@scala-steward.org>
2021-02-12 00:25:38 +0100

    Update testcontainers to 1.15.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
   }
 
   object testcontainers {
-    val testcontainersVersion    = "1.15.0"
+    val testcontainersVersion    = "1.15.2"
     val testcontainers           = "org.testcontainers" % "testcontainers" % testcontainersVersion
     val testcontainersLocalStack = "org.testcontainers" % "localstack" % testcontainersVersion
     val testcontainersKafka      = "org.testcontainers" % "kafka" % testcontainersVersion

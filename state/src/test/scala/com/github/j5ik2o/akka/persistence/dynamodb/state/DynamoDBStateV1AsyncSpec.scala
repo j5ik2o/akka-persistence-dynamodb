13f0c0704e9652cb87a141458ef5e922def05e2d
Junichi Kato <j5ik2o@gmail.com>
2022-06-09 08:51:28 +0900

    test: Add the test for DynamoDBState V1 Async.

---
+++ state/src/test/scala/com/github/j5ik2o/akka/persistence/dynamodb/state/DynamoDBStateV1AsyncSpec.scala
@@ -0,0 +1,62 @@
+package com.github.j5ik2o.akka.persistence.dynamodb.state
+
+import akka.persistence.state.DurableStateStoreRegistry
+import com.github.j5ik2o.akka.persistence.dynamodb.config.client.{ClientType, ClientVersion}
+import com.github.j5ik2o.akka.persistence.dynamodb.state.scaladsl.{DynamoDBDurableStateStoreV1, StateSpecBase}
+import com.github.j5ik2o.akka.persistence.dynamodb.utils.{ConfigHelper, DynamoDBSpecSupport, RandomPortUtil}
+import org.scalatest.concurrent.ScalaFutures
+import org.testcontainers.DockerClientFactory
+
+import scala.concurrent.duration.DurationInt
+
+object DynamoDBStateV1AsyncSpec {
+  val dynamoDBHost: String = DockerClientFactory.instance().dockerHostIpAddress()
+  val dynamoDBPort: Int    = RandomPortUtil.temporaryServerPort()
+}
+
+class DynamoDBStateV1AsyncSpec  extends StateSpecBase(
+  ConfigHelper
+    .config(
+      Some("state-reference"),
+      legacyConfigFormat = false,
+      legacyJournalMode = false,
+      dynamoDBHost = DynamoDBStateV1AsyncSpec.dynamoDBHost,
+      dynamoDBPort = DynamoDBStateV1AsyncSpec.dynamoDBPort,
+      clientVersion = ClientVersion.V1.toString,
+      clientType = ClientType.Async.toString
+    )
+)
+  with ScalaFutures
+  with DynamoDBSpecSupport {
+
+  implicit val pc: PatienceConfig = PatienceConfig(30.seconds, 1.seconds)
+
+  override protected lazy val dynamoDBPort: Int = DynamoDBStateV1AsyncSpec.dynamoDBPort
+
+  "A durable state store plugin" - {
+    "instantiate a JdbcDurableDataStore successfully" in {
+      val store = DurableStateStoreRegistry
+        .get(system)
+        .durableStateStoreFor[DynamoDBDurableStateStoreV1[String]](DynamoDBDurableStateStoreProvider.Identifier)
+
+      store.upsertObject("1", 1, "ABC", "").futureValue()
+      val result = store.getObject("1").futureValue()
+
+      result.value shouldBe Some("ABC")
+
+      store shouldBe a[DynamoDBDurableStateStoreV1[_]]
+      store.system.settings.config shouldBe system.settings.config
+    }
+  }
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    createTable()
+  }
+
+  override def afterAll(): Unit = {
+    deleteTable()
+    super.afterAll()
+  }
+
+}

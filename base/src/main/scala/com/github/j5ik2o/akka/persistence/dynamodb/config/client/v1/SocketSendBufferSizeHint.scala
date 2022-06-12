61cbe8b7cdf148baefb9229e122029a57b86824e
Junichi Kato <j5ik2o@gmail.com>
2022-06-12 09:01:59 +0900

    refactor: add `final` keyword

--- base/src/main/scala/com/github/j5ik2o/akka/persistence/dynamodb/config/client/v1/SocketSendBufferSizeHint.scala
+++ base/src/main/scala/com/github/j5ik2o/akka/persistence/dynamodb/config/client/v1/SocketSendBufferSizeHint.scala
@@ -1,3 +1,3 @@
 package com.github.j5ik2o.akka.persistence.dynamodb.config.client.v1
 
-case class SocketSendBufferSizeHint(send: Int, receive: Int)
+final case class SocketSendBufferSizeHint(send: Int, receive: Int)

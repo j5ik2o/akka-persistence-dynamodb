7bbdd0729b6a79bc3dee3c000b0e439a06680215
Junichi Kato <j5ik2o@gmail.com>
2022-06-12 22:46:43 +0900

    add license headers

--- base/src/main/scala/com/github/j5ik2o/akka/persistence/dynamodb/model/PersistenceId.scala
+++ base/src/main/scala/com/github/j5ik2o/akka/persistence/dynamodb/model/PersistenceId.scala
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2020 Junichi Kato
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.j5ik2o.akka.persistence.dynamodb.model
 
 final class PersistenceId(private val value: String) {
-  require(value.length >= 1 && value.length <= 2048, "Invalid string length")
+  require(value.nonEmpty && value.length <= 2048, "Invalid string length")
 
   override def toString: String = s"PersistenceId($value)"
 

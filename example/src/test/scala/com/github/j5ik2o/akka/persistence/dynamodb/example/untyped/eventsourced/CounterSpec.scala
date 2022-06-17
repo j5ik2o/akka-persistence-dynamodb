45fd5a573f5a889d52cdf5d1fd38bed4ca2facf6
Junichi Kato <j5ik2o@gmail.com>
2022-06-17 11:52:37 +0900

    test: add tests

--- example/src/test/scala/com/github/j5ik2o/akka/persistence/dynamodb/example/untyped/eventsourced/CounterSpec.scala
+++ example/src/test/scala/com/github/j5ik2o/akka/persistence/dynamodb/example/untyped/eventsourced/CounterSpec.scala
@@ -2,12 +2,11 @@ package com.github.j5ik2o.akka.persistence.dynamodb.example.untyped.eventsourced
 
 import akka.actor.{ ActorRef, ActorSystem }
 import akka.testkit.{ ImplicitSender, TestKit, TestProbe }
-import com.amazonaws.services.dynamodbv2.model.{ AttributeValue, QueryRequest }
+import com.amazonaws.services.dynamodbv2.model.{ AttributeValue, QueryRequest, QueryResult }
 import com.github.j5ik2o.akka.persistence.dynamodb.config.client.{ ClientType, ClientVersion }
 import com.github.j5ik2o.akka.persistence.dynamodb.example.untyped.eventsourced.CounterProtocol.GetValueReply
 import com.github.j5ik2o.akka.persistence.dynamodb.journal.config.JournalPluginConfig
 import com.github.j5ik2o.akka.persistence.dynamodb.utils.{ ConfigHelper, DynamoDBSpecSupport, RandomPortUtil }
-import com.typesafe.config.Config
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.freespec.AnyFreeSpecLike
@@ -19,37 +18,42 @@ import scala.jdk.CollectionConverters._
 
 object CounterSpec {
   val dynamoDBHost: String = DockerClientFactory.instance().dockerHostIpAddress()
-  val dynamoDBPort: Int    = RandomPortUtil.temporaryServerPort()
-  val legacyJournalMode    = false
-  val softDelete           = false
 
-  val config: Config = ConfigHelper
-    .config(
-      Some("persistence-reference"),
-      legacyConfigFormat = false,
-      legacyJournalMode = CounterSpec.legacyJournalMode,
-      dynamoDBHost = CounterSpec.dynamoDBHost,
-      dynamoDBPort = CounterSpec.dynamoDBPort,
-      clientVersion = ClientVersion.V1.toString,
-      clientType = ClientType.Async.toString,
-      softDelete = softDelete
-    )
+  val legacyJournalMode = false
 }
 
-class CounterSpec
-    extends TestKit(ActorSystem("CounterSpec", CounterSpec.config))
+abstract class BaseCounterSpec(
+    _dynamoDBPort: Int,
+    softDelete: Boolean,
+    clientVersion: ClientVersion.Value,
+    clientType: ClientType.Value
+) extends TestKit(
+      ActorSystem(
+        "CounterSpec",
+        ConfigHelper
+          .config(
+            Some("persistence-reference"),
+            legacyConfigFormat = false,
+            legacyJournalMode = CounterSpec.legacyJournalMode,
+            dynamoDBHost = CounterSpec.dynamoDBHost,
+            dynamoDBPort = _dynamoDBPort,
+            clientVersion = clientVersion.toString,
+            clientType = clientType.toString,
+            softDelete = softDelete
+          )
+      )
+    )
     with AnyFreeSpecLike
     with BeforeAndAfterAll
     with ImplicitSender
     with ScalaFutures
     with DynamoDBSpecSupport {
-
   val testTimeFactor: Int = sys.env.getOrElse("TEST_TIME_FACTOR", "1").toInt
   logger.debug(s"testTimeFactor = $testTimeFactor")
 
   implicit val pc: PatienceConfig = PatienceConfig(30.seconds, 1.seconds)
 
-  override protected lazy val dynamoDBPort: Int = CounterSpec.dynamoDBPort
+  override protected lazy val dynamoDBPort: Int = _dynamoDBPort
 
   override val legacyJournalTable: Boolean = CounterSpec.legacyJournalMode
 
@@ -67,6 +71,36 @@ class CounterSpec
     actors.foreach { actorRef => system.stop(actorRef) }
   }
 
+  def getJournalRows(actorId: UUID, pluginConfig: JournalPluginConfig): QueryResult = {
+    val persistenceId  = Counter.pid(actorId)
+    val fromSequenceNr = 0
+    val toSequenceNr   = Long.MaxValue
+    val deleted        = None
+    val limit          = Int.MaxValue
+    val queryRequest = new QueryRequest()
+      .withTableName(journalTableName)
+      .withIndexName(
+        GetJournalRowsIndexName
+      ).withKeyConditionExpression(
+        "#pid = :pid and #snr between :min and :max"
+      ).withFilterExpression(deleted.map { _ => s"#flg = :flg" }.orNull)
+      .withExpressionAttributeNames(
+        (Map(
+          "#pid" -> pluginConfig.columnsDefConfig.persistenceIdColumnName,
+          "#snr" -> pluginConfig.columnsDefConfig.sequenceNrColumnName
+        ) ++ deleted
+          .map(_ => Map("#flg" -> pluginConfig.columnsDefConfig.deletedColumnName)).getOrElse(Map.empty)).asJava
+      )
+      .withExpressionAttributeValues(
+        (Map(
+          ":pid" -> new AttributeValue().withS(persistenceId),
+          ":min" -> new AttributeValue().withN(fromSequenceNr.toString),
+          ":max" -> new AttributeValue().withN(toSequenceNr.toString)
+        ) ++ deleted.map(b => Map(":flg" -> new AttributeValue().withBOOL(b))).getOrElse(Map.empty)).asJava
+      ).withLimit(limit)
+    val result = dynamoDBClient.query(queryRequest)
+    result
+  }
   "CounterSpec" - {
     "increment & getValue" in {
       val testProbe = TestProbe()
@@ -80,7 +114,7 @@ class CounterSpec
         counterRef ! CounterProtocol.Increment // seqNr = 4, 1, saveSnapshot(4)
 
         counterRef ! CounterProtocol.GetValue(testProbe.ref)
-        val reply = testProbe.expectMsgType[CounterProtocol.GetValueReply]((10 * testTimeFactor).seconds)
+        val reply = testProbe.expectMsgType[GetValueReply]((10 * testTimeFactor).seconds)
         assert(reply.n == 4)
 
         counterRef ! CounterProtocol.SaveSnapshot(testProbe.ref)
@@ -102,37 +136,12 @@ class CounterSpec
         assert(reply.n == 1)
       }
 
-      val pluginConfig = JournalPluginConfig.fromConfig(CounterSpec.config.getConfig("j5ik2o.dynamo-db-journal"))
+      val pluginConfig = JournalPluginConfig.fromConfig(system.settings.config.getConfig("j5ik2o.dynamo-db-journal"))
 
-      val persistenceId  = Counter.pid(actorId)
-      val fromSequenceNr = 0
-      val toSequenceNr   = Long.MaxValue
-      val deleted        = None
-      val limit          = Int.MaxValue
-      val queryRequest = new QueryRequest()
-        .withTableName(journalTableName)
-        .withIndexName(
-          GetJournalRowsIndexName
-        ).withKeyConditionExpression(
-          "#pid = :pid and #snr between :min and :max"
-        ).withFilterExpression(deleted.map { _ => s"#flg = :flg" }.orNull)
-        .withExpressionAttributeNames(
-          (Map(
-            "#pid" -> pluginConfig.columnsDefConfig.persistenceIdColumnName,
-            "#snr" -> pluginConfig.columnsDefConfig.sequenceNrColumnName
-          ) ++ deleted
-            .map(_ => Map("#flg" -> pluginConfig.columnsDefConfig.deletedColumnName)).getOrElse(Map.empty)).asJava
-        )
-        .withExpressionAttributeValues(
-          (Map(
-            ":pid" -> new AttributeValue().withS(persistenceId),
-            ":min" -> new AttributeValue().withN(fromSequenceNr.toString),
-            ":max" -> new AttributeValue().withN(toSequenceNr.toString)
-          ) ++ deleted.map(b => Map(":flg" -> new AttributeValue().withBOOL(b))).getOrElse(Map.empty)).asJava
-        ).withLimit(limit)
-      val result = dynamoDBClient.query(queryRequest)
+      val result: QueryResult = getJournalRows(actorId, pluginConfig)
 
       assert(result.getCount > 0)
+
       val items = result.getItems.asScala.map(_.asScala).toVector
       items.foreach { item =>
         println(
@@ -146,7 +155,7 @@ class CounterSpec
         )
       }
 
-      if (!CounterSpec.softDelete) {
+      if (!softDelete) {
         assert(items.size == 1)
         assert(items(0)("persistence-id").getS == Counter.pid(actorId))
         assert(items(0)("sequence-nr").getN.toInt == 4)
@@ -166,5 +175,122 @@ class CounterSpec
       }
     }
   }
+}
+
+// ---
+
+package softdelete {
+
+  object CounterV1AsyncSpec {
+    val dynamoDBPort: Int = RandomPortUtil.temporaryServerPort()
+  }
+
+  class CounterV1AsyncSpec
+      extends BaseCounterSpec(
+        CounterV1AsyncSpec.dynamoDBPort,
+        softDelete = true,
+        clientVersion = ClientVersion.V1,
+        clientType = ClientType.Async
+      )
+
+  // ---
+
+  object CounterV1SyncSpec {
+    val dynamoDBPort: Int = RandomPortUtil.temporaryServerPort()
+  }
+
+  class CounterV1SyncSpec
+      extends BaseCounterSpec(
+        CounterV1SyncSpec.dynamoDBPort,
+        softDelete = true,
+        clientVersion = ClientVersion.V1,
+        clientType = ClientType.Sync
+      )
+
+  // ---
+
+  object CounterV2AsyncSpec {
+    val dynamoDBPort: Int = RandomPortUtil.temporaryServerPort()
+  }
+
+  class CounterV2AsyncSpec
+      extends BaseCounterSpec(
+        CounterV2AsyncSpec.dynamoDBPort,
+        softDelete = true,
+        clientVersion = ClientVersion.V2,
+        clientType = ClientType.Async
+      )
+
+  // ---
+
+  object CounterV2SyncSpec {
+    val dynamoDBPort: Int = RandomPortUtil.temporaryServerPort()
+  }
+
+  class CounterV2SyncSpec
+      extends BaseCounterSpec(
+        CounterV2SyncSpec.dynamoDBPort,
+        softDelete = true,
+        clientVersion = ClientVersion.V2,
+        clientType = ClientType.Sync
+      )
+
+}
+
+package harddelete {
+
+  object CounterV1AsyncSpec {
+    val dynamoDBPort: Int = RandomPortUtil.temporaryServerPort()
+  }
+
+  class CounterV1AsyncSpec
+      extends BaseCounterSpec(
+        CounterV1AsyncSpec.dynamoDBPort,
+        softDelete = false,
+        clientVersion = ClientVersion.V1,
+        clientType = ClientType.Async
+      )
+
+  // ---
+
+  object CounterV1SyncSpec {
+    val dynamoDBPort: Int = RandomPortUtil.temporaryServerPort()
+  }
+
+  class CounterV1SyncSpec
+      extends BaseCounterSpec(
+        CounterV1SyncSpec.dynamoDBPort,
+        softDelete = false,
+        clientVersion = ClientVersion.V1,
+        clientType = ClientType.Sync
+      )
+
+  // ---
+
+  object CounterV2AsyncSpec {
+    val dynamoDBPort: Int = RandomPortUtil.temporaryServerPort()
+  }
+
+  class CounterV2AsyncSpec
+      extends BaseCounterSpec(
+        CounterV2AsyncSpec.dynamoDBPort,
+        softDelete = false,
+        clientVersion = ClientVersion.V2,
+        clientType = ClientType.Async
+      )
+
+  // ---
+
+  object CounterV2SyncSpec {
+    val dynamoDBPort: Int = RandomPortUtil.temporaryServerPort()
+  }
+
+  class CounterV2SyncSpec
+      extends BaseCounterSpec(
+        CounterV2SyncSpec.dynamoDBPort,
+        softDelete = false,
+        clientVersion = ClientVersion.V2,
+        clientType = ClientType.Sync
+      )
 
 }

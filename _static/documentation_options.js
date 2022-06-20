b7d79dd6b7573f10a129623ef44fd128c21d223e
Junichi Kato <j5ik2o@gmail.com>
2022-06-20 18:33:50 +0900

    updated site

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.11.5+14-d4a3df3d+20220620-1825-SNAPSHOT',
+    VERSION: '1.11.5+14-d4a3df3d+20220620-1833-SNAPSHOT',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
